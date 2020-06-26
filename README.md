2609b372a0012b9262a1b1cc1a726dc815cd9e2d
Hugh Brown <hugh@balena.io>
2020-06-26 13:54:41 -0700

    Initial commit
    Change-type: patch
    Signed-off-by: Hugh Brown <hugh@balena.io>

---
+++ README.md
@@ -0,0 +1,8 @@
+# alog
+
+An experiment to see if I can tail an application's logs in a terminal
+with the balena SDK.
+
+# License
+
+Apache 2.0; see LICENSE.
---
+++ bin/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const getSdk = require('balena-sdk');
+const balena = getSdk({
+  apiUrl: "https://api.balena-cloud.com/"
+});
+
+console.log("Hello, world!");
+
+var personalToken = fs.readFileSync('/home/hugh/.balena/token.personal', 'utf8');
+
+balena.auth.loginWithToken(personalToken, function(error) {
+  if (error) throw error;
+})
+
+balena.auth.whoami()
+  .then(username => {
+    if(username) {
+      console.log("I am", username);
+    } else {
+      console.log("I am nobody? I guess?")
+    }
+  })
+
+balena.models.device.getAllByApplication('arduino-wx-logger').then(devices => {
+  // I've only got one device
+  return devices[0].uuid;
+})
+  .then(uuid => {
+    return balena.logs.subscribe(uuid);
+  })
+  .then(logs => {
+    logs.on('line', function(line){
+      console.log(line);
+    } )
+  })
