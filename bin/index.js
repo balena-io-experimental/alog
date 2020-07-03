f8e90e87e183fd091399850a599dce0eaef8e200
Hugh Brown <hugh@balena.io>
2020-07-03 11:21:47 -0700

    Add yargs and reorganize order of functions a bit
    Change-type: patch
    Signed-off-by: Hugh Brown <hugh@balena.io>

--- bin/index.js
+++ bin/index.js
@@ -1,30 +1,23 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const yargs = require('yargs');
 const logutil = require('./logutil');
 
-const getSdk = require('balena-sdk');
-const balena = getSdk({
-  apiUrl: "https://api.balena-cloud.com/"
-});
-
-var personalToken = fs.readFileSync('/home/hugh/.balena/token.personal', 'utf8');
-
-balena.auth.loginWithToken(personalToken, function(error) {
-  if (error) throw error;
-})
-
-balena.auth.whoami()
-  .then(username => {
-    if(username) {
-      console.log("I am", username);
-    } else {
-      console.log("I am nobody? I guess?")
-    }
-  })
+const options = yargs
+      .usage("Usage: -u [uuid of device] -a [application]")
+      .option("u", {alias: "uuid",
+		    describe: "UUID of device",
+		    type: "string",
+		    demandOption: true})
+      .option("a", {alias: "application",
+		    describe: "Application name",
+		    type: "string",
+		    demandOption: true})
+      .argv;
 
 async function getDeviceServicesIds(device) {
-  return await balena.models.device.getWithServiceDetails(myDevice)
+  return await balena.models.device.getWithServiceDetails(device)
     .then(device => {
       allServices = {};
       deviceServices = device.current_services;
@@ -38,7 +31,7 @@ async function getDeviceServicesIds(device) {
 }
 
 async function subscribeToLogs(application) {
-  return await balena.models.device.getAllByApplication(myApplication).then(devices => {
+  return await balena.models.device.getAllByApplication(application).then(devices => {
     // I've only got one device
     return devices[0].uuid;
   })
@@ -47,10 +40,34 @@ async function subscribeToLogs(application) {
     })
 }
 
-subscribeToLogs(myApplication)
+// Set up the client...
+const getSdk = require('balena-sdk');
+const balena = getSdk({
+  apiUrl: "https://api.balena-cloud.com/"
+});
+
+var personalToken = fs.readFileSync('/home/hugh/.balena/token.personal', 'utf8');
+
+balena.auth.loginWithToken(personalToken, function(error) {
+  if (error) throw error;
+})
+
+// Canary to make sure it works...
+balena.auth.whoami()
+  .then(username => {
+    if(username) {
+      console.log("I am", username);
+    } else {
+      console.log("I am nobody? I guess?")
+    }
+  })
+
+// ...then try subscribing to some logs.
+subscribeToLogs(options.application)
   .then(logs => {
-    getDeviceServicesIds(myDevice).then(data => allServiceIds = data);
-    logs.on('line', function(line){
+    getDeviceServicesIds(options.devices)
+      .then(data => allServiceIds = data);
+    logs.on('line', function(line) {
       logutil.dispatch(line, allServiceIds);
     })
   });
